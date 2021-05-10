76889fd3b7487e566ef37de6e205f1c740f3996f
kamukaz <64062673+kamukaz@users.noreply.github.com>
2021-05-10 23:46:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-try:
-    import sys
-    import os
-    import time
-    import hashlib
-    import binascii
-    import multiprocessing
-    from multiprocessing import Process, Queue
-    from multiprocessing.pool import ThreadPool
-    import threading
-    import base58
-    import ecdsa
-    import requests
-    from hashing import*
+
+import sys
+import os
+import time
+import hashlib
+import binascii
+import multiprocessing
+from multiprocessing import Process, Queue
+from multiprocessing.pool import ThreadPool
+import threading
+import base58
+import ecdsa
+import requests
     
 file = open("found.txt","a")
 file.write("address: " + "\n" +
